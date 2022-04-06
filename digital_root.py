b89bef1959a98229c8f51bf6782388807b355655
FelixTEK <62253541+FelixTEK@users.noreply.github.com>
2022-04-06 20:57:00 +0700

    Update digital_root.py

--- digital_root.py
+++ digital_root.py
@@ -13,35 +13,34 @@ def datatypsel(): #data type selection
   global orignum
   global numtyp
   global digits
+  num = 0
   num = input("Please insert your number.\n> ") #desired value
   orignum = num
   num = num.lower()
   digits = list(num)
-  if ("x" in digits) or ("a" in digits or "b" in digits or "c" in digits or "d" in digits or "e" in digits or "f" in digits):
-    print("Your value is a hexadecimal value.")
-    if ("x" not in digits):
-      num = num + "x"
-    num = int(num, 16)
+  if "a" in digits or "b" in digits or "c" in digits or "d" in digits or "e" in digits or "f" in digits:
+    try:
+      num = int(float(num))
+      num = str(num)
+      print("Your value is a hexadecimal value.")
+    except ValueError:
+      print("Invalid input. Please type again.\n")
+      datatypsel()
     numtyp = "hex"
 
-  elif ("o" in digits) and ("8" not in digits and "9" not in digits):
-    print("Your value is an octal value.")
-    num = int(num, 8)
-    numtyp = "oct"
-
-  elif ("b" in digits) and ("8" not in digits and "9" not in digits and "7" not in digits and "6" not in digits and "5" not in digits and "4" not in digits and "3" not in digits and "2" not in digits):
-    print("Your value is a binary value.")
-    num = int(num, 2)
-    numtyp = "bin"  
-  
   elif ("8" not in digits and "9" not in digits) and ("7" in digits or "6" in digits or "5" in digits or "4" in digits or "3" in digits or "2" in digits):
+    try:
+      num = int(float(num))
+      num = str(num)
+    except ValueError:
+      print("Invalid input. Please type again.\n")
+      num = str(num)
+      datatypsel()
     numtyp = input("Is this a hexadecimal number, an octal number, or a decimal number? [HEX/DEC/OCT]\n> ").lower()
     if numtyp == "hex":
-        num = ("0x"+num)
         num = int(num, 16)
         print("Your value is a hexadecimal value.")
     elif numtyp == "oct":
-        num = ("0o"+num)
         num = int(num, 8)
         print("Your value is an octal value.")
     elif numtyp == "dec":
@@ -49,26 +48,36 @@ def datatypsel(): #data type selection
         print("Your value is a decimal value.")
   
   elif "8" not in digits and "9" not in digits and "7" not in digits and "6" not in digits and "5" not in digits and "4" not in digits and "3" not in digits and "2" not in digits:
+    try:
+      num = int(float(num))
+      num = str(num)
+    except ValueError:
+      print("Invalid input. Please type again.\n")
+      num = str(num)
+      datatypsel()
     numtyp = input("Is this a hexadecimal number, an octal number, a decimal number, or a binary number? (int)? [HEX/DEC/OCT/BIN]\n> ").lower()
     if numtyp == "hex":
-        num = ("0x"+num)
         num = int(num, 16)
         print("Your value is a hexadecimal value.")
     elif numtyp == "oct":
-        num = ("0o"+num)
         num = int(num, 8)
         print("Your value is an octal value.")
     elif numtyp == "dec":
         num = int(num)
         print("Your value is a decimal value.")
     elif numtyp == "bin":
-        num = ("0b"+num)
         num = int(num, 2)
         print("Your value is a binary value.")
   else:
+    try:
+      num = int(float(num))
+      num = str(num)
+    except ValueError:
+      print("Invalid input. Please type again.\n")
+      num = str(num)
+      datatypsel()
     numtyp = input("Is this a hexadecimal number, or a decimal number (normal int)? [HEX/DEC]\n> ").lower()
     if numtyp == "hex":
-        num = ("0x"+num)
         num = int(num, 16)
         print("Your value is a hexadecimal value.")
     elif numtyp == "dec":
